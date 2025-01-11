a9b1604898f613488a04c55a2820ce915daa7d5f
Ajwwkxkdang <107187521+Ajwyunsx@users.noreply.github.com>
2025-01-12 03:00:48 +0800

    Add files via upload

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -1,9 +1,8 @@
 package;
 
 #if android
-import android.Tools;
+import android.os.Environment;
 import android.Permissions;
-import android.PermissionsList;
 #end
 import lime.app.Application;
 import openfl.events.UncaughtErrorEvent;
@@ -27,6 +26,8 @@ class SUtil
 {
 	#if android
 	private static var aDir:String = null; // android dir
+	public static final READ_EXTERNAL_STORAGE:String = 'android.permission.READ_EXTERNAL_STORAGE';
+	public static final WRITE_EXTERNAL_STORAGE:String = 'android.permission.WRITE_EXTERNAL_STORAGE';
 	#end
 
 	public static function getPath():String
@@ -35,7 +36,7 @@ class SUtil
 		if (aDir != null && aDir.length > 0)
 			return aDir;
 		else
-			return aDir = Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file') + '/';
+			return aDir = Environment.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file') + '/';
 		#else
 		return '';
 		#end
@@ -44,16 +45,16 @@ class SUtil
 	public static function doTheCheck()
 	{
 		#if android
-		if (!Permissions.getGrantedPermissions().contains(PermissionsList.READ_EXTERNAL_STORAGE) || !Permissions.getGrantedPermissions().contains(PermissionsList.WRITE_EXTERNAL_STORAGE))
+		if (!Permissions.getGrantedPermissions().contains(READ_EXTERNAL_STORAGE) || !Permissions.getGrantedPermissions().contains(WRITE_EXTERNAL_STORAGE))
 		{
-			Permissions.requestPermissions([PermissionsList.READ_EXTERNAL_STORAGE, PermissionsList.WRITE_EXTERNAL_STORAGE]);
+			Permissions.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
 			SUtil.applicationAlert('Permissions', "if you acceptd the permissions all good if not expect a crash" + '\n' + 'Press Ok to see what happens');
 		}
 
-		if (Permissions.getGrantedPermissions().contains(PermissionsList.READ_EXTERNAL_STORAGE) || Permissions.getGrantedPermissions().contains(PermissionsList.WRITE_EXTERNAL_STORAGE))
+		if (Permissions.getGrantedPermissions().contains(READ_EXTERNAL_STORAGE) || Permissions.getGrantedPermissions().contains(WRITE_EXTERNAL_STORAGE))
 		{
-			if (!FileSystem.exists(Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file')))
-				FileSystem.createDirectory(Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file'));
+			if (!FileSystem.exists(Environment.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file')))
+				FileSystem.createDirectory(Environment.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file'));
 
 			if (!FileSystem.exists(SUtil.getPath() + 'assets') && !FileSystem.exists(SUtil.getPath() + 'mods'))
 			{
@@ -124,7 +125,7 @@ class SUtil
 
 	private static function applicationAlert(title:String, description:String)
 	{
-		Application.current.window.alert(description, title);
+		android.Tools.showAlertDialog(title, description, {name: "OK", func: null}, null);
 	}
 
 	#if android
